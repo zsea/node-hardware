7f58a908197d746c7d1670f1851a120424189d1d
zsea <zsea@qq.com>
2024-05-26 21:22:33 +0800

    support electron

--- setup.js
+++ setup.js
@@ -1,16 +1,28 @@
 const Downloader = require("nodejs-file-downloader");
-const arch = process.arch
-    ,os = require('os')
-    ,log = require('single-line-log').stdout
-;
-const fileUrl=`https://raw.githubusercontent.com/zsea/node-hardware-bin/master/hardware-${os.platform()}-${process.arch}.node`;
+const os = require('os')
+    , path = require("path")
+    , fs = require("fs").promises
+    , log = require('single-line-log').stdout
+    ;
+let pre = "node";
 
 (async () => {
+    const packagePath = path.join(process.env["INIT_CWD"], "package.json");
+    const package = require(packagePath);
+    
+    
+    if ((package.devDependencies && package.devDependencies.electron)
+        || (package.dependencies && package.dependencies.electron)) {
+        pre = "electron";
+    }
+    //throw new Error(pre);
+    const fileUrl = `https://raw.githubusercontent.com/zsea/node-hardware-bin/master/${pre}-hardware-${os.platform()}-${process.arch}.node`;
     const downloader = new Downloader({
         url: fileUrl,
-        directory: "./", 
-        cloneFiles:false,
-        proxy:process.env["npm_config_https_proxy"],
+        directory: "./",
+        cloneFiles: false,
+        fileName:`hardware-${os.platform()}-${process.arch}.node`,
+        proxy: process.env["npm_config_https_proxy"],
         onProgress: function (percentage, chunk, remainingSize) {
             log(`Downloading ${percentage}% ${fileUrl}\n`);
         },
@@ -22,7 +34,7 @@ const fileUrl=`https://raw.githubusercontent.com/zsea/node-hardware-bin/master/h
     } catch (error) {
         console.log(error);
     }
-    finally{
-        
+    finally {
+
     }
 })();
